d2610195b5c1fd62ec1a1b1d42f94ac881e268d3
Nunex <74361788+NunexGTX@users.noreply.github.com>
2026-05-03 10:33:16 +0100

    gracefuly exited loop

--- SoundscapeVReapy.py
+++ SoundscapeVReapy.py
@@ -1,20 +1,52 @@
 #import reapy
 import asyncio
+import sys
 from SoundscapeVRCommunications.SoundscapeVRCommunicationsService import SoundscapeVRCommunicationsService
 from jsonUtils.SoundscapeReapyControllerConfig import SoundscapeReapyControllerConfig
 
 #Reaper project
-#project = reapy.Project()
+project = None
+config = None
+communicator = None
+
+def ReapyInit():
+    #Open Reapy
+
+    #Open the template project on reapy
+
+    #Get that project
+    #project = reapy.Project()
+    pass
+
+def Reset():
+    pass
+
+def Exit(communicator: SoundscapeVRCommunicationsService):
+    print()
+    print("Exitting...")
+    communicator.disconnect()
+    print("Goodbye...")
+    #sys.exit(0) #Stop execution
+    #----END----
 
 async def main():
     config = SoundscapeReapyControllerConfig()
     communicator = SoundscapeVRCommunicationsService(config)
 
-    await communicator.start() # blocks until Unity connects
-
-    while True:
-        request = await communicator.receiveRequest()
-        print(request)
+    try:
+        await communicator.start() # blocks until Unity connects
+        while True:
+            request = await communicator.receiveRequest()
+            await communicator.sendResponse("OK")
+    except asyncio.CancelledError:
+        Exit(communicator)
+    finally:
+        pass
+            
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    ReapyInit()
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
